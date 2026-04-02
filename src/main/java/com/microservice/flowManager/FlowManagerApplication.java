57f0fed19b72ab939a4c8f0d9315769cf584251a
gkwgu <polina.petr0v4@yandex.ru>
2026-04-02 11:39:01 +0300

    flowManager microservice

---
+++ src/main/java/com/microservice/flowManager/FlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.flowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/flowManager/FlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.flowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
